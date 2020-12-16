c8f5878b8b5f75e6a8793f5e84706d33959d002f
홍가영 <thehd9891@imform.co.kr>
2020-12-16 16:06:37 +0900

    vue class binding

---
+++ tutorial/201216/js/main.js
@@ -0,0 +1,33 @@
+var vm1 = new Vue({
+  el: '#example1',
+  data: {
+    isActive: true,
+    hasError: false,
+  },
+});
+
+var vm2 = new Vue({
+  el: '#example2',
+  data: {
+    classObject: {
+      active: true,
+      'text-danger': true
+    }
+  },
+});
+
+var vm3 = new Vue({
+  el: '#example3',
+  data: {
+    isActive: true,
+    error: null
+  },
+  computed: {
+    classObject: function () {
+      return {
+        active: this.isActive && !this.error,
+        'text-danger': this.error && this.error.type === 'fatal'
+      }
+    }
+  }
+});
